32450ac1b0b1b3fa1a8f4362159c71c7c642a271
IrakliK <ikhizani@lion.lmu.edu>
2017-01-29 20:39:24 -0800

    Reconstructed server.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const mongo = require('mongodb');
 const app = express();
 const router = express.Router();
 const path = require('path');
@@ -9,20 +16,57 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ouraudio');
 const db = mongoose.connection;
 
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    const namespace = param.split('.');
+    const root = namespace.shift();
+    let formParam = root;
+    while (namespace.length) {
+      formParam += `[ ${root} ]`;
+    }
+    return {
+      param: formParam,
+      msg,
+      value,
+    };
+  },
+}));
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
 
-router.post('/quotes', (req) => {
-  console.log(req.body);
-});
+app.use('/', routes);
+app.use('/users', users);
 
-app.use('/', router);
+app.set('port', (process.env.PORT || 3000));
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), () => {
+  console.log(`Server started on port ${app.get('port')}`);
 });
