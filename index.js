d9dcfc2ccd158a794f4c2e21ad6983fd6c9e4125
IrakliK <ikhizani@lion.lmu.edu>
2017-02-06 19:03:30 -0800

    Added logo and now can work with images.

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: 'secret',
